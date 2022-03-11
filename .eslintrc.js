55ae6f91abf98927e946a94732ec67a65dff92a5
emanu7478 <emanu7478@gmail.com>
2022-03-11 21:58:29 +0530

    add path aliad and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,26 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      'babel-module': {
+        root: ['.'],
+        alias: require('./aliases.json'),
+      },
+    },
+  },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 8,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', '@typescript-eslint'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -10,6 +28,16 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'import/no-unresolved': 2,
+        'no-console': 2,
+        'promise/prefer-await-to-then': 2,
+        'promise/no-nesting': 2,
+        'promise/prefer-await-to-callbacks': 2,
+        'react-native/no-unused-styles': 2,
+        'react-native/split-platform-components': 1,
+        'react-native/no-inline-styles': 1,
+        'react-native/no-raw-text': 1,
+        'require-await': 2,
       },
     },
   ],
